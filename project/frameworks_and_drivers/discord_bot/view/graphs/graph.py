876bff07d2007653a2b9e6ced596de22161a3211
erick <erick1204200100@gmail.com>
2026-06-20 14:23:38 -0300

    Style of the graphs is better

---
+++ project/frameworks_and_drivers/discord_bot/view/graphs/graph.py
@@ -0,0 +1,7 @@
+class Graph:
+
+    BACKGROUND_COLOR: str = "#0E0E0E"
+    FOREGROUND_COLOR: str = "#1B1B1B"
+    WEAK_COLOR: str = "#138E92"
+    STRONG_COLOR: str = "#14FFEC"
+    EDGE_COLOR: str = "#FFFFFF"
--- project/frameworks_and_drivers/discord_bot/view/graphs/members.py
+++ project/frameworks_and_drivers/discord_bot/view/graphs/members.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
 from project.domain.interfaces.graphs_interface.curve_interface import GraphCurveInterface
 from typing import List, Tuple
+from project.frameworks_and_drivers.discord_bot.view.graphs.graph import Graph
 
-class MembersGraph(GraphCurveInterface):
+class MembersGraph(GraphCurveInterface, Graph):
 
     REPO_PATH: str = "project/img_repo"
-    
+
     @classmethod
     def build_curve_qtt(cls, days: List[str], qtts: List[int], server_id: int, author_id: int) -> None:
         days_eq: List[int] = [pos + 1 for pos in range(len(days))]
         plt.style.use("dark_background") #<--- setting the style of the graph
-        plt.plot(days_eq, qtts, color = "#86BBFF", linewidth = 2) #<--- Plotting the graph
+        plt.rcParams["figure.facecolor"] = cls.BACKGROUND_COLOR
+        plt.rcParams["axes.facecolor"] = cls.FOREGROUND_COLOR
+        plt.plot(days_eq, qtts, color = cls.WEAK_COLOR, linewidth = 2) #<--- Plotting the graph
         plt.xlabel("Time (days)")
         plt.ylabel("Quantity of members")
         plt.savefig(cls.REPO_PATH + f"/members_{server_id}{author_id}.png", dpi = 150)
@@ -23,8 +26,10 @@ class MembersGraph(GraphCurveInterface):
         range_qtt: List[int] = [qtt for qtt in range(from_qtt, until_qtt + 1)]
         range_prob: List[float] = probs[from_qtt : until_qtt + 1]
         plt.style.use("dark_background")
-        plt.bar(qtts, probs, width = 1, color = "#86BBFF", edgecolor = "#AFF6FF", linewidth = 2.5, zorder = 0)
-        plt.bar(range_qtt, range_prob, width = 1, color = "#A100C9", edgecolor = "#AFF6FF", linewidth = 2.5, zorder = 1)
+        plt.rcParams["figure.facecolor"] = cls.BACKGROUND_COLOR
+        plt.rcParams["axes.facecolor"] = cls.FOREGROUND_COLOR
+        plt.bar(qtts, probs, width = 1, color = cls.WEAK_COLOR, edgecolor = cls.EDGE_COLOR, linewidth = 2.5, zorder = 0)
+        plt.bar(range_qtt, range_prob, width = 1, color = cls.STRONG_COLOR, edgecolor = cls.EDGE_COLOR, linewidth = 2.5, zorder = 1)
         plt.xlabel("quantity of entrances")
         plt.ylabel("Probability (%)")
         plt.savefig(cls.REPO_PATH + f"/members_{server_id}{author_id}_poisson.png", dpi = 150)
